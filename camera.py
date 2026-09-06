416e348d9c68efba3e0afc643b9e704d1289953e
Arthutstuts96 <oarthurlima1575@gmail.com>
2026-09-06 12:40:08 -0300

    Salvando frame e separando canais de cor

--- camera.py
+++ camera.py
@@ -1,7 +1,12 @@
 import cv2
 import time
 
-from separar_cor import separar_canais_rgb
+from separar_cor import separar_canais_rgb, visualizar_canais
+
+def plotar_grafico(frame):
+    separar_canais_rgb(frame)
+    visualizar_canais(frame, f"./imagens/graficos/frame_{int(time.time())}.png")
+
 
 cap = cv2.VideoCapture(0)
 prev_time = time.time()
@@ -10,18 +15,23 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
+
     curr_time = time.time()
     fps = 1.0 / (curr_time - prev_time) if curr_time != prev_time else 0.0
     prev_time = curr_time
 
     cv2.putText(frame, f'FPS: {fps:.1f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow("Câmera Aberta", frame)
+    cv2.imshow('Câmera Aberta', frame)
 
-    separar_canais_rgb(frame)
-    
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q') or key == ord('Q'):
         break
+
+    if key == ord('s') or key == ord('S'):
+        caminho = f'./imagens/screenshots/captura_{int(time.time())}.png'
+        cv2.imwrite(caminho, frame)
+        print(f'Print salvo em {caminho}.')
+        plotar_grafico(frame)
 
 cap.release()
 cv2.destroyAllWindows()
--- separar_cor.py
+++ separar_cor.py
@@ -17,7 +17,7 @@ def separar_canais_rgb(img_bgr):
     return vermelho, verde, azul
 
 # — FUNÇÃO: visualizar canais —
-def visualizar_canais(img_bgr):
+def visualizar_canais(img_bgr, caminho):
     """Plota a imagem original e seus 3 canais R, G, B lado a lado."""
     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
     r, g, b = separar_canais_rgb(img_bgr)
@@ -28,34 +28,29 @@ def visualizar_canais(img_bgr):
     axes[2].imshow(g, cmap="gray");  axes[2].set_title("Canal G (Verde)")
     axes[3].imshow(b, cmap="gray");  axes[3].set_title("Canal B (Azul)")
     for ax in axes: ax.axis("off")
-    plt.tight_layout(); plt.show()
+    plt.tight_layout()
+    plt.savefig(caminho)
 
 # Entendendo vetores
-imageColor = cv2.imread("./cereja.jpeg", cv2.IMREAD_COLOR)  # ----------------------
-imageBlack = cv2.imread("./cereja.jpeg", cv2.IMREAD_GRAYSCALE)
+# imageColor = cv2.imread(r"./imagens/teste/cereja.jpeg", cv2.IMREAD_COLOR)
+# imageBlack = cv2.imread(r"./imagens/teste/cereja.jpeg", cv2.IMREAD_GRAYSCALE)
 
-print("-> Conteúdo numérico da imagem imageBlack.png (escala de cinza)")
-print("Forma do vetor:", imageBlack.shape)
-print("Primeiros 3x3 pixels:\n", imageBlack[:3, :3])
+# print("-> Conteúdo numérico da imagem imageBlack.png (escala de cinza)")
+# print("Forma do vetor:", imageBlack.shape)
+# print("Primeiros 3x3 pixels:\n", imageBlack[:3, :3])
 
-print("\n -- \n")
+# print("\n -- \n")
 
-print("-> Conteúdo numérico da imagem imageColor.png (colorida)")
-print("Forma do vetor:", imageColor.shape)
-print("Primeiros 3x3 pixels (BGR):\n", imageColor[:3, :3, :])
+# print("-> Conteúdo numérico da imagem imageColor.png (colorida)")
+# print("Forma do vetor:", imageColor.shape)
+# print("Primeiros 3x3 pixels (BGR):\n", imageColor[:3, :3, :])
 
-# Exibição de canais
-imageColor = cv2.imread("img/cereja.jpeg")
-visualizar_canais(imageColor)
-imageColor = cv2.imread("img/cereja.png")
-visualizar_canais(imageColor)
-imageColor = cv2.imread("img/connor-ward.png")
-visualizar_canais(imageColor)
-imageColor = cv2.imread("img/laranja.png")
-visualizar_canais(imageColor)
-imageColor = cv2.imread("img/GuardaSol.png")
-visualizar_canais(imageColor)
+# # Exibição de canais
+# imageColor = cv2.imread(r"./imagens/teste/cereja.jpeg")
+# visualizar_canais(imageColor)
 
 '''
 PEGAR, NESSE VÍDEO, 1 FRAME, E DENTRO DESSE FRAME PEGAR E APLICAR ESSAS TÉCNICAS DESSE ARQUIVO
+-> Separar canais RGB
+-> Plotar gráfico com a imagem original e seus 3 canais
 '''
